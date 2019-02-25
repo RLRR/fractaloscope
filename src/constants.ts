09ab505b43c174036e750d34d1c9b12bcbc7ffd1
Alex Fedosov <osvodef@gmail.com>
2019-02-25 20:11:23 +0300

    tweak color constants, remove more palettes

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const textureSize = 9;
 
-export const paletteDuration = 5000;
-export const paletteChangeDuration = 2000;
+export const paletteDuration = 8000;
+export const paletteChangeDuration = 3000;
 
 export const touchpadScrollZoomSpeed = 0.0025;
 export const touchpadGestureZoomSpeed = 0.025;
